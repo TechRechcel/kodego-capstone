96b0c9fcd2689867c9a6de7b7a7e50512ec8c26a
TechRechcel <rechceltoledo@gmail.com>
2023-01-16 23:24:40 +0800

    added controllers

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Department;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -9,17 +9,11 @@ class ProjectController extends Controller
     //
 
 
-    public function getAllDepartments(Request $request)
-    {
-        $departments = Department::all();
-        return view('index', ('departments') -> $departments);
-    }
-    public function myBookings(Request $request)
-    {
-        $user = Auth::user();
-        $bookings = Booking::where('user_id',Auth::user()->id)->get();
-        return \view('myBookings', ['bookings' => $bookings]);
-    }
+
+public function getAllDepartments(Request $request) {
+    $departments = Department::all();
+    return view('index', ['departments'=>$departments]);
+}
 
 }
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,26 @@
 @extends('layouts.main')
 
 @section('content')
-    <h2>Our Content</h2>
-    <p>Welcome to the example app.</p>
+
+<div class="container-lg" style='margin: 0 auto;'>
+    <div class="row mt-5 ms-5">
+
+        @foreach($departments as $department)
+
+        <div class="col-lg-4 col-md-4 col-sm-12 text-center mb-3">
+            <div class="card" style="width: 18rem">
+            <img src="{{$departments->image}}" alt="" style="width: 200px;margin: 0 auto">
+            <div class="card-body">
+                <div class="card-title">{{ $department->name}}</div>
+                <div class="card-text">{{ $department->description}}</div>
+            </div>
+            </div>
+        </div>
+
+
+    </div>
+</div>
+@endforeach
+
 
 @endsection
